7ce16c87f027099e9dfe96db834524f66449257e
E.A <emanuel.alvz@gmail.com>
2022-09-19 23:58:26 -0300

    Primera version del Proyecto Entregable

---
+++ AppCoder/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=60)
+    camada = models.IntegerField()
+
+class Estudiante(models.Model):
+
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=60)
+    correo = models.EmailField()
+    
---
+++ AppCoder/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from AppCoder.models import *
+from django.http import HttpResponse
+
+# Create your views here.
+
+def inicio(request):
+    return HttpResponse("vista inicio")
+
+def curso(request):
+
+    curso2 = Curso(nombre="Diseno", camada=656)
+    curso2.save()
+
+    return HttpResponse(f"Estoy guardando el primer curso: {curso2.nombre}")
